fe9b905579379cf2ab277096dce5a09cb504bbd2
cc_du <cc_du@outlook.com>
2026-08-04 12:35:51 +0800

    Fix install.sh to use absolute symlinks and echo each link

--- install.sh
+++ install.sh
@@ -3,11 +3,15 @@
 # Windows (Git Bash/MSYS) needs this for symlinks
 [[ "$(uname -s)" == *MINGW* ]] && export MSYS=winsymlinks:nativestrict
 
-link() { ln --symbolic --force "$@"; }
+DIR=$(cd "$(dirname "$0")" && pwd)
+link() { 
+    ln --symbolic --force --verbose "$DIR/$1" "$2" 
+}
 
 # bash
 link bash/bashrc ~/.bashrc
 link bash/inputrc ~/.inputrc
+link lesskey ~/.lesskey
 
 # git
 link git/git-amend.sh ~/.git-amend.sh
@@ -15,19 +19,17 @@ link git/gitattributes ~/.gitattributes
 link git/gitconfig ~/.gitconfig
 
 # vim
+mkdir -p ~/.vim
 link vim/plugins.vim ~/.vim/plugins.vim
 link vim/vimrc ~/.vimrc
 link idea/ideavimrc ~/.ideavimrc
 
 # rime
-mkdir -p ~/.vim "${APPDATA}/Rime"
+mkdir -p "${APPDATA}/Rime"
 link Rime/custom_phrase.txt "${APPDATA}/Rime/custom_phrase.txt"
 link Rime/default.custom.yaml "${APPDATA}/Rime/default.custom.yaml"
 link Rime/rime_ice.custom.yaml "${APPDATA}/Rime/rime_ice.custom.yaml"
 link Rime/weasel.custom.yaml "${APPDATA}/Rime/weasel.custom.yaml"
-
-# less
-link lesskey ~/.lesskey
 
 # tmux
 link tmux.conf ~/.tmux.conf
